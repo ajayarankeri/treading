08e11cafc8a5253a5767ef263bbaa7e925c5cbf2
ajayarankeri <ajayarankeri@gmail.com>
2019-07-29 13:52:53 +0530

    initial commit

--- src/main/java/com/hcl/treading/controller/TreadingController.java
+++ src/main/java/com/hcl/treading/controller/TreadingController.java
@@ -1,5 +1,12 @@
 package com.hcl.treading.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class TreadingController {
+	
+	
 
 }
--- src/main/java/com/hcl/treading/repository/TreadingRepository.java
+++ src/main/java/com/hcl/treading/repository/TreadingRepository.java
@@ -1,8 +1,11 @@
 package com.hcl.treading.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hcl.treading.entity.StockTransaction;
+
 @Repository
-public interface TreadingRepository {
+public interface TreadingRepository extends JpaRepository<StockTransaction, Long>{
 
 }
