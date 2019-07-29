7d90c2b0213f5e0e7d80c927a5769f3d0d7264d5
ajayarankeri <ajayarankeri@gmail.com>
2019-07-29 14:03:33 +0530

    changes committed

--- src/main/java/com/hcl/treading/service/TreadingService.java
+++ src/main/java/com/hcl/treading/service/TreadingService.java
@@ -2,7 +2,11 @@ package com.hcl.treading.service;
 
 import org.springframework.stereotype.Service;
 
+import com.hcl.treading.dto.StockPurchaseDto;
+
 @Service
 public interface TreadingService {
+
+	void purchaseStock(StockPurchaseDto stockPurchaseDto);
 
 }
--- src/main/java/com/hcl/treading/serviceimpl/TreadingServiceImpl.java
+++ src/main/java/com/hcl/treading/serviceimpl/TreadingServiceImpl.java
@@ -1,6 +1,19 @@
 package com.hcl.treading.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class TreadingServiceImpl {
+import com.hcl.treading.dto.StockPurchaseDto;
+import com.hcl.treading.repository.TreadingRepository;
+import com.hcl.treading.service.TreadingService;
+
+public class TreadingServiceImpl implements TreadingService{
+	
+	@Autowired
+	TreadingRepository treadingRepository;
+
+	@Override
+	public void purchaseStock(StockPurchaseDto stockPurchaseDto) {
+		
+	}
 
 }
